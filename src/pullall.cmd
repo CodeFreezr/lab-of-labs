92104fe89438e23ef29d0af359e3a87fd15d27cc
CodeFreezr <burkhardt@web.de>
2018-03-05 21:58:46 +0100

    new stuff

--- src/pullall.cmd
+++ src/pullall.cmd
@@ -1,9 +1,9 @@
 pushd .
-cd .. && git pull && cd ..
-cd api-lab && git pull && cd ..
-cd graphi-lab && git pull && cd ..
-cd gro-lab && git pull && cd ..
-cd snippets && git pull && cd ..
-cd twitterei && git pull && cd ..
-cd vue-lab && git pull
+cd .. && call git pull && cd ..
+cd api-lab && call git pull && cd ..
+cd graphi-lab && call git pull && cd ..
+cd gro-lab && call git pull && cd ..
+cd snippets && call git pull && cd ..
+cd twitterei && call git pull && cd ..
+cd vue-lab && call git pull
 popd
---
+++ src/pushall.cmd
@@ -0,0 +1,9 @@
+pushd .
+cd .. && call git push && cd ..
+cd api-lab && call git push && cd ..
+cd graphi-lab && call git push && cd ..
+cd gro-lab && call git push && cd ..
+cd snippets && call git push && cd ..
+cd twitterei && call git push && cd ..
+cd vue-lab && call git push
+popd

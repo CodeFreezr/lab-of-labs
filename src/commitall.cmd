f3d88d2289b0aca8c68a6a94b8c387aa794917cb
CodeFreezr <burkhardt@web.de>
2018-03-05 21:56:54 +0100

    new stuff

---
+++ src/commitall.cmd
@@ -0,0 +1,22 @@
+pushd .
+cd .. 
+call git commit . -m "new stuff"
+cd ..
+cd api-lab 
+call git commit . -m "new stuff" 
+cd ..
+cd graphi-lab 
+call git commit . -m "new stuff" 
+cd ..
+cd gro-lab 
+call git commit . -m "new stuff" 
+cd ..
+cd snippets 
+call git commit . -m "new stuff" 
+cd ..
+cd twitterei 
+call git commit . -m "new stuff" 
+cd ..
+cd vue-lab 
+call git commit . -m "new stuff"
+popd
